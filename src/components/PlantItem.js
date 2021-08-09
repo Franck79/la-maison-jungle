3c9912ba99a9b3bd0d2fe780e3c26ac048eedfc8
Franck <fmoeuf@gmail.com>
2021-08-09 15:52:39 +0200

    add and updates files

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,16 +1,14 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function handleClick(e) {
-    console.log('✨ Ceci est mon event :', e)
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function PlantItem(props) {
-    const { id, cover, name, water, light, title } = props
-
+function PlantItem({ cover, name, water, light, price }) {
 	return (
-		<li key={id} className='lmj-plant-item' onClick={handleClick}>
-            <h1>{title}</h1>
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
